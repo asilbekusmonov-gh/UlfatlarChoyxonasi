3388f8918349911e94ac012a92893773c004f306
Usmonov Asilbek <asilbekusmonovv17@gmail.com>
2026-06-05 18:43:23 +0500

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,28 +2,21 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Loyihaning asosiy (ildiz) papkasini avtomatik hisoblash
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# .env faylidan muhit o'zgaruvchilarini yuklash
+
 load_dotenv(dotenv_path=BASE_DIR / ".env")
 
-# ==========================================
-# 1. Maxfiy Kalitlar (O'qish va Tekshirish)
-# ==========================================
 BOT_TOKEN: str = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN topilmadi! Iltimos, .env faylini tekshiring.")
 
-# Telegram ID-lar raqam bo'lgani uchun uni integer (int) turiga o'giramiz
+
 ADMIN_GROUP_ID: int = int(os.getenv("ADMIN_GROUP_ID", 0))
 
 
-# ==========================================
-# 2. Restoran Statik Sozlamalari
-# ==========================================
 CURRENCY = "UZS"
-ITEMS_PER_PAGE = 6  # Kelajakda menyu kattalashsa, sahifalarga bo'lish (pagination) uchun
+ITEMS_PER_PAGE = 6 
 
 
 RESTAURANT_INFO = {
